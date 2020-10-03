f659e71336277c56846e6789cc2835a39c815cde
Yu-Song Syu <kuma@simbo.com.tw>
2020-10-03 16:08:30 +0800

    ok case

--- src/test/java/idv/kuma/amazing/register/RegisterControllerTest.java
+++ src/test/java/idv/kuma/amazing/register/RegisterControllerTest.java
@@ -5,20 +5,34 @@ import org.assertj.core.api.Assertions;
 import org.junit.Test;
 import org.mockito.Mockito;
 
-import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.Mockito.when;
 
 public class RegisterControllerTest {
 
     @Test
+    public void When_All_OK_Then_Throws_Exception_Then_Return_Response() throws RegisterException {
+
+        RegisterForm registerForm = new RegisterForm();
+        RegisterService registerService = Mockito.mock(RegisterService.class);
+        when(registerService.register(registerForm)).thenReturn(new SuccessResponse("FAKE_TOKEN"));
+
+        Response actual = new RegisterController(registerService).greeting(registerForm);
+
+        Assertions.assertThat(actual)
+                .isEqualToComparingFieldByField(new SuccessResponse("FAKE_TOKEN"));
+
+
+    }
+
+
+    @Test
     public void When_RegisterService_Throws_Exception_Then_Return_ErrorResponse() throws RegisterException {
 
-        RegisterService registerService = Mockito.mock(RegisterService.class);
-        when(registerService.register(any(RegisterForm.class))).thenThrow(new RegisterException("FAKE_MESSAGE"));
-
-        RegisterController controller = new RegisterController(registerService);
         RegisterForm registerForm = new RegisterForm();
-        Response actual = controller.greeting(registerForm);
+        RegisterService registerService = Mockito.mock(RegisterService.class);
+        when(registerService.register(registerForm)).thenThrow(new RegisterException("FAKE_MESSAGE"));
+
+        Response actual = new RegisterController(registerService).greeting(registerForm);
 
         Assertions.assertThat(actual)
                 .isEqualToComparingFieldByField(new ErrorResponse("FAKE_MESSAGE"));
