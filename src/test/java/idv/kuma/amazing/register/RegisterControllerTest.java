f80fbf88fd92dd7a734272fd659b2a9f75f2c4b5
Yu-Song Syu <kuma@simbo.com.tw>
2020-10-03 16:12:07 +0800

    refactor: reposition expresions

--- src/test/java/idv/kuma/amazing/register/RegisterControllerTest.java
+++ src/test/java/idv/kuma/amazing/register/RegisterControllerTest.java
@@ -13,10 +13,11 @@ public class RegisterControllerTest {
     public void When_All_OK_Then_Throws_Exception_Then_Return_Response() throws RegisterException {
 
         RegisterForm registerForm = new RegisterForm();
-        RegisterService registerService = Mockito.mock(RegisterService.class);
-        when(registerService.register(registerForm)).thenReturn(new SuccessResponse("FAKE_TOKEN"));
 
-        Response actual = new RegisterController(registerService).greeting(registerForm);
+        RegisterService mockedService = Mockito.mock(RegisterService.class);
+        when(mockedService.register(registerForm)).thenReturn(new SuccessResponse("FAKE_TOKEN"));
+
+        Response actual = new RegisterController(mockedService).greeting(registerForm);
 
         Assertions.assertThat(actual)
                 .isEqualToComparingFieldByField(new SuccessResponse("FAKE_TOKEN"));
@@ -29,10 +30,11 @@ public class RegisterControllerTest {
     public void When_RegisterService_Throws_Exception_Then_Return_ErrorResponse() throws RegisterException {
 
         RegisterForm registerForm = new RegisterForm();
-        RegisterService registerService = Mockito.mock(RegisterService.class);
-        when(registerService.register(registerForm)).thenThrow(new RegisterException("FAKE_MESSAGE"));
 
-        Response actual = new RegisterController(registerService).greeting(registerForm);
+        RegisterService mockedService = Mockito.mock(RegisterService.class);
+        when(mockedService.register(registerForm)).thenThrow(new RegisterException("FAKE_MESSAGE"));
+
+        Response actual = new RegisterController(mockedService).greeting(registerForm);
 
         Assertions.assertThat(actual)
                 .isEqualToComparingFieldByField(new ErrorResponse("FAKE_MESSAGE"));
