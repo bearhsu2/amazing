6fb0547c67734814661c163ae8c4bea305aa5f10
Yu-Song Syu <kuma@simbo.com.tw>
2020-10-04 10:00:17 +0800

    refactor test: introduce fields

--- src/test/java/idv/kuma/amazing/register/service/RegisterServiceTest.java
+++ src/test/java/idv/kuma/amazing/register/service/RegisterServiceTest.java
@@ -4,6 +4,7 @@ import idv.kuma.amazing.RegisterException;
 import idv.kuma.amazing.register.controller.RegisterForm;
 import org.assertj.core.api.Assertions;
 import org.junit.Assert;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.mockito.Mockito;
 
@@ -12,20 +13,28 @@ import static org.mockito.Mockito.when;
 
 public class RegisterServiceTest {
 
+
+    private RegisterForm form;
+    public FormChecker mockedChecker;
+    public Registerer mockedRegisterer;
+    public TokenGenerator mockedTokenGenerator;
+    public Messenger mockedMessenger;
+
+
+    @BeforeEach
+    void setUp() {
+        mockedChecker = Mockito.mock(FormChecker.class);
+        mockedRegisterer = Mockito.mock(Registerer.class);
+        mockedTokenGenerator = Mockito.mock(TokenGenerator.class);
+        mockedMessenger = Mockito.mock(Messenger.class);
+    }
+
+
     @Test
     public void When_Checker_Fails_Then_Throw_Exception() throws CheckerException {
 
-        RegisterForm form = new RegisterForm();
-
-
-        FormChecker mockedChecker = Mockito.mock(FormChecker.class);
+        form = new RegisterForm();
         doThrow(new CheckerException("FAKE_MESSAGE")).when(mockedChecker).check(form);
-
-        Registerer mockedRegisterer = Mockito.mock(Registerer.class);
-        TokenGenerator mockedTokenGenerator = Mockito.mock(TokenGenerator.class);
-        when(mockedTokenGenerator.generate()).thenReturn("FAKE_TOKEN");
-        Messenger mockedMessenger = Mockito.mock(Messenger.class);
-
 
         RegisterService service = new RegisterService(
                 mockedChecker,
@@ -48,14 +57,9 @@ public class RegisterServiceTest {
     @Test
     public void When_All_Ok_Then_Return_Token() throws RegisterException {
 
-        RegisterForm form = new RegisterForm();
-
-
-        FormChecker mockedChecker = Mockito.mock(FormChecker.class);
-        Registerer mockedRegisterer = Mockito.mock(Registerer.class);
-        TokenGenerator mockedTokenGenerator = Mockito.mock(TokenGenerator.class);
+        form = new RegisterForm();
         when(mockedTokenGenerator.generate()).thenReturn("FAKE_TOKEN");
-        Messenger mockedMessenger = Mockito.mock(Messenger.class);
+
         RegisterService service = new RegisterService(
                 mockedChecker,
                 mockedRegisterer,
