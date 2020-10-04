f493c560c1c95ffd101e5d6318d89e224ad81752
Yu-Song Syu <kuma@simbo.com.tw>
2020-10-04 18:25:41 +0800

    use switch case instead of if-else clause

--- src/main/java/idv/kuma/amazing/register/controller/RegisterServiceFactory.java
+++ src/main/java/idv/kuma/amazing/register/controller/RegisterServiceFactory.java
@@ -3,30 +3,34 @@ package idv.kuma.amazing.register.controller;
 import idv.kuma.amazing.register.service.*;
 import org.springframework.stereotype.Component;
 
-import java.util.Objects;
-
 @Component
 public class RegisterServiceFactory {
 
     public RegisterService create(Type type) throws RegisterServiceFactoryException {
 
-
-        if (Objects.equals(type, Type.EMAIL)) {
-            return new RegisterService(
-                    new DataChecker(),
-                    new EmailRegisterer(),
-                    new TokenGenerator(),
-                    new EmailMessenger()
-            );
-        } else if (Objects.equals(type, Type.PHONE)) {
-            return new RegisterService(
-                    new DataChecker(),
-                    new PhoneRegisterer(),
-                    new TokenGenerator(),
-                    new PhoneMessenger()
-            );
+        if (type == null) {
+            throw new RegisterServiceFactoryException("Type unsupported.");
         }
 
-        throw new RegisterServiceFactoryException("Type unsupported.");
+        switch (type) {
+            case EMAIL:
+                return new RegisterService(
+                        new DataChecker(),
+                        new EmailRegisterer(),
+                        new TokenGenerator(),
+                        new EmailMessenger()
+                );
+            case PHONE:
+                return new RegisterService(
+                        new DataChecker(),
+                        new PhoneRegisterer(),
+                        new TokenGenerator(),
+                        new PhoneMessenger()
+                );
+            default:
+                throw new RegisterServiceFactoryException("Type unsupported.");
+        }
+
+
     }
 }
